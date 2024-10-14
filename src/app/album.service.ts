e7d3e31183e36c895704c44bdc44374f92150381
Alan Moore <alan@olpt.net>
2024-10-14 16:11:59 -0700

    feat: filling in AlbumService with getAlbums and create Album model.

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, Observable } from 'rxjs';
+import { Album } from './album';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AlbumService {
+  private albumsUrl = 'http://localhost:8080/albums';
 
-  constructor() { }
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
+
+  constructor(private http: HttpClient) {}
+
+  getAlbums(): Observable<Album[]> {
+    return this.http
+      .get<Album[]>(this.albumsUrl)
+      .pipe(catchError(this.handleError<Album[]>('getAlbums', [])));
+  }
+  
+  handleError<T>(arg0: string, arg1: never[]): (err: any, caught: Observable<Album[]>) => import("rxjs").ObservableInput<any> {
+    throw new Error('Method not implemented.');
+  }
 }
