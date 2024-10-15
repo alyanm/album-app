f400f29f47eb23eb510a9dfc643af397996170ef
Alan Moore <alan@olpt.net>
2024-10-15 11:14:29 -0700

    feat: implement album list view, import HttpClient provider

--- src/app/album-list/album-list.component.ts
+++ src/app/album-list/album-list.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { AlbumService } from '../album.service';
+import { Album } from '../album';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-album-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './album-list.component.html',
-  styleUrl: './album-list.component.css'
+  styleUrl: './album-list.component.css',
 })
 export class AlbumListComponent {
+  albums: Album[] = [];
 
+  constructor(private albumService: AlbumService) {}
+
+  ngOnInit() {
+    this.albumService.getAlbums().subscribe((albums) => {
+      this.albums = albums;
+    });
+  }
 }
--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -1,5 +1,5 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { catchError, Observable } from 'rxjs';
 import { Album } from './album';
 
@@ -7,21 +7,28 @@ import { Album } from './album';
   providedIn: 'root',
 })
 export class AlbumService {
+  private http = inject(HttpClient);
   private albumsUrl = 'http://localhost:8080/albums';
 
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   getAlbums(): Observable<Album[]> {
     return this.http
       .get<Album[]>(this.albumsUrl)
       .pipe(catchError(this.handleError<Album[]>('getAlbums', [])));
   }
-  
-  handleError<T>(arg0: string, arg1: never[]): (err: any, caught: Observable<Album[]>) => import("rxjs").ObservableInput<any> {
+
+  handleError<T>(
+    arg0: string,
+    arg1: never[]
+  ): (
+    err: any,
+    caught: Observable<Album[]>
+  ) => import('rxjs').ObservableInput<any> {
     throw new Error('Method not implemented.');
   }
 }
